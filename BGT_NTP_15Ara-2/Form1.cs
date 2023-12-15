8a58451077992ea79575086483bb82a5f88a2b29
Lab1-1 <Lab1-1@DESKTOP-NHSN8JG>
2023-12-15 11:28:17 +0300

    Proje dosyası ekle.

---
+++ BGT_NTP_15Ara-2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BGT_NTP_15Ara_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<harcama> harcamalar = new List<harcama>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            harcama _harcama1 = new harcama();
+            _harcama1.IslemAdi = textBox1.Text;
+            _harcama1.IslemTuru = textBox2.Text;    
+            _harcama1.Tutar=Convert.ToInt32(textBox3.Text);
+            harcamalar.Add(_harcama1);
+            label6.Text = harcamalar.Count.ToString();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int tutar = 0;
+            for (int i = 0; i < harcamalar.Count; i++)
+            {
+                listBox1.Items.Add(harcamalar[i].IslemAdi + "  " + harcamalar[i].Tutar);
+                tutar += harcamalar[i].Tutar;
+            }
+            label7.Text = tutar.ToString();
+        }
+    }
+}
---
+++ BGT_NTP_15Ara-2/harcama.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGT_NTP_15Ara_2
+{
+    class harcama
+    {
+        string _islemAdi;
+        string _islemTuru;
+        int _tutar;
+
+        public string IslemAdi
+        {
+            get { return _islemAdi; }   
+            set { _islemAdi = value;}
+        }
+        public string IslemTuru
+        {
+            get { return _islemTuru;}
+            set { _islemTuru = value;}  
+        }
+        public int Tutar
+        {
+            get { return _tutar; }  
+            set { _tutar = value;}  
+        }
+    }
+}
